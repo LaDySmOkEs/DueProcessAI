4d6c4fd4c0a15a796858816145118226663e3314
LaDySmOkEs <panterailovephil@gmail.com>
2025-05-20 08:20:11 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // <- this should be the root directory if index.html is in the root
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
   }
 })
